52343c2015d08e2eb88eb7887fdbfa347ffd90bf
Memory1031 <50623917+Memory1031@users.noreply.github.com>
2020-02-24 14:11:07 +0800

    完成了按照特定排序显示数据

--- src/components/statisticList/statisticList.vue
+++ src/components/statisticList/statisticList.vue
@@ -4,8 +4,19 @@
       <div class="button">
         <DatePicker type="date" size="large" :value="selectedDate" @on-change="changeDate"
                     :options="options" placeholder="选择日期" style="width: 200px"></DatePicker>
+        <Select v-model="sortBy" style="width:200px" size="large">
+          <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+        </Select>
+        <RadioGroup v-model="sortWay" size="large">
+          <Radio :label="1">
+            <span>从低到高</span>
+          </Radio>
+          <Radio :label="-1">
+            <span>从高到低</span>
+          </Radio>
+        </RadioGroup>
         <Input size="large" placeholder="输入地级市名称（选填）"
-               style="width: 250px" clearable
+               style="width: 200px" clearable
                v-model="cityName"></Input>
         <Button type="success" class="refresh_button" @click="searchForApply">筛选</Button>
       </div>
@@ -16,30 +27,79 @@
         no-data-text="当前没有会议室申请"
         stripe border
         :loading="loading"
-        height="700"
-        :columns="columns" :data="data"></Table>
+        height="650"
+        :columns="columns" :data="nowData"></Table>
+      <Page :total="data.length" :page-size="10" @on-change="changepage" styles="text-align: center"
+            show-total show-elevator/>
     </Row>
   </div>
 </template>
 
 <script>
     import axios from 'axios'
+    import expandRow from './table-expand'
+
     export default {
         name: 'statisticList',
+        components: { expandRow },
         data() {
             return{
                 loading: false,
+                loading_nowData: false,
                 cityName: '',
                 yesterday: new Date(new Date().setDate(new Date().getDate() - 1)),
                 selectedDate: '',
+                dataCount: 0,//总条数
+                pageCurrent: 1,//当前页
+                nowData: [],
                 options: {
                     disabledDate (date) {
                         return date && date.valueOf() > Date.now();
                     }
                 },
+                cityList: [
+                    {
+                        value: '0',
+                        label: '确诊人数'
+                    },
+                    {
+                        value: '1',
+                        label: '治愈人数'
+                    },
+                    {
+                        value: '2',
+                        label: '死亡人数'
+                    },
+                    {
+                        value: '3',
+                        label: '治愈率'
+                    },
+                    {
+                        value: '4',
+                        label: '死亡率'
+                    },
+                    {
+                        value: '5',
+                        label: '每百万人确诊人数'
+                    }
+                ],
+                sortBy: '0',
+                sortWay: 1,
                 columns: [
                     {
-                        type: 'index',
+                        type: 'expand',
+                        width: 50,
+                        render: (h, params) => {
+                            return h(expandRow, {
+                                props: {
+                                    row: params.row,
+                                }
+                            })
+                        }
+                    },
+                    {
+                        title: '#',
+                        key: 'number',
                         width: 100,
                         align: 'center'
                     },
@@ -47,7 +107,7 @@
                         title: '省/直辖市',
                         key: 'provincename',
                         align: 'center',
-                        width: 200,
+                        width: 150,
                         tooltip: true
                     },
                     {
@@ -62,42 +122,36 @@
                         key: 'totalconfirm',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                     {
                         title: '治愈人数',
                         key: 'totalheal',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                     {
                         title: '死亡人数',
                         key: 'totaldead',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                     {
                         title: '治愈率',
                         key: 'cureRate',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                     {
                         title: '死亡率',
                         key: 'mortality',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                     {
                         title: '每百万人感染人数',
                         key: 'confirmInMillion',
                         align: 'center',
                         tooltip: true,
-                        sortable: true
                     },
                 ],
                 data: [],
@@ -113,17 +167,36 @@
             init(index,type){
                 this.data = []
                 this.loading = true;
+                this.loading_nowData = true;
+                this.nowData = [];
                 axios({
-                    url: apiRoot + '/epidemic/allAreaInfo?date=' + this.selectedDate,
-                    method: 'get'
+                    url: apiRoot + '/epidemic/allAreaInfo',
+                    method: 'post',
+                    data: {
+                        date: this.selectedDate,
+                        isUp: this.sortWay,
+                        order: parseInt(this.sortBy)
+                    }
                 }).then((res) => {
                     if(res.data.code == 200){
+                        let nowData_full = false;
+                        let num = 1
                         res.data.data.forEach((item) => {
                             if(type == '' || item.provincename.indexOf(type) != -1 || item.cityname.indexOf(type) != -1)
                             {
+                                item.newParams = 'number';
+                                item['number'] = num;
+                                num++;
                                 this.data.push(item)
                             }
+                            if(this.data.length == 10){
+                                this.nowData = this.data.slice(0, 10);
+                                nowData_full = true
+                            }
                         })
+                        if(nowData_full == false){
+                            this.nowData = this.data
+                        }
                         this.$Message.success(index);
                         console.log(this.data)
                         this.loading = false;
@@ -143,7 +216,17 @@
                 let date1 = new Date(newDate)
                 this.selectedDate = date1.getFullYear() +  "-" + (date1.getMonth()> 9 ? (date1.getMonth() + 1) : "0" +
                     (date1.getMonth() + 1)) + "-" +(date1.getDate()> 9 ? (date1.getDate()) : "0" + (date1.getDate()));
-            }
+            },
+            changepage(index) {
+                //需要显示开始数据的index,(因为数据是从0开始的，页码是从1开始的，需要-1)
+                let _start = (index - 1) * 10;
+                //需要显示结束数据的index
+                let _end = index * 10;
+                //截取需要显示的数据
+                this.nowData = this.data.slice(_start, _end);
+                //储存当前页
+                this.pageCurrent = index;
+            },
         }
     }
 </script>
---
+++ src/components/statisticList/table-expand.vue
@@ -0,0 +1,40 @@
+<style scoped>
+  .expand-row{
+    margin-bottom: 16px;
+  }
+  .hr_extend{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    color: #d7dde4;
+  }
+</style>
+<template>
+  <div>
+    <Card style="font-size: 15px">
+      <p>11123</p>
+    </Card>
+  </div>
+</template>
+<script>
+    import axios from 'axios'
+
+    export default {
+        props: {
+            row: Object
+        },
+        data(){
+            return{
+                conference_info: { },
+                picture_exist: false
+            }
+        },
+        mounted(){
+            this.init();
+        },
+        methods:{
+            init(){
+
+            }
+        }
+    };
+</script>
